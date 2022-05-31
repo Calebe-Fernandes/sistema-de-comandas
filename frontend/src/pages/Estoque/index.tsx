f8ca1da5f6af045e8c535d65d9080eb1cd24f9e5
felipekaua~ <fkauadelima@gmail.com>
2022-05-30 21:49:59 -0300

    [Front]Padronização-do-codigo

--- frontend/src/pages/Estoque/index.tsx
+++ frontend/src/pages/Estoque/index.tsx
@@ -19,14 +19,7 @@ type drink = {
     "stockAmmount": number,
     "alcoholic": boolean
 }
-var requestDrink = {
-    "id": 0,
-    "price": 0,
-    "productName": "a",
-    "description": "a",
-    "stockAmmount": 0,
-    "alcoholic": false
-}
+
 type food = {
     "id": number,
     "price": number,
@@ -34,7 +27,6 @@ type food = {
     "description": string,
     "isAvaliable": boolean
   }
-
 
 const Estoque: React.FC = () => {
 
@@ -44,54 +36,55 @@ const Estoque: React.FC = () => {
     var [showDrinks, setShowDrinks] = useState<boolean>(true);
     var [waitingApiResponse, setWaitingApiResponse] = useState<boolean>(true);
 
-
     useEffect(() => {
+
         api.get("/drinks")
             .then(response => {
               setDrinks(response.data);
             })
             .catch(error => {
-              console.log(error)
+              console.log(error);
             })
             .then( () => {
               setWaitingApiResponse(false);
             }
             )
-      },[])
+    },[])
 
-      useEffect(()=>{
-        document.getElementById('drinkList')?.classList.add('hidden') ;
-        document.getElementById('foodList')?.classList.add('hidden');
-         if(!waitingApiResponse){
-          if(!showDrinks){
-            foods.length > 0 && document.getElementById('foodList')?.classList.remove('hidden');
-          }else if(showDrinks){
-            drinks.length > 0 && document.getElementById('drinkList')?.classList.remove('hidden'); 
-          }
-         }
-       },[showDrinks,waitingApiResponse])
+    useEffect(()=>{
 
-       function getFoodList(){
-    
-        var activeButton = $('.menu-option')[1];
-        $('.menu-option').removeClass('active');
-        $(activeButton).addClass('active');
-    
-        setWaitingApiResponse(true);
-        setShowDrinks(false);
-    
-        api.get("/food")
-            .then(response => {
-              setFoods(response.data);
-            })
-            .catch(error => {
-              console.log(error)
-            })
-            .then( () => {
-              setWaitingApiResponse(false);
+    document.getElementById('drinkList')?.classList.add('hidden') ;
+    document.getElementById('foodList')?.classList.add('hidden');
+        if(!waitingApiResponse){
+            if(!showDrinks){
+                foods.length > 0 && document.getElementById('foodList')?.classList.remove('hidden');
+            }else if(showDrinks){
+                drinks.length > 0 && document.getElementById('drinkList')?.classList.remove('hidden'); 
             }
-            )
-      }
+        }
+    },[showDrinks,waitingApiResponse])
+
+    function getFoodList(){
+
+    var activeButton = $('.menu-option')[1];
+    $('.menu-option').removeClass('active');
+    $(activeButton).addClass('active');
+
+    setWaitingApiResponse(true);
+    setShowDrinks(false);
+
+    api.get("/food")
+        .then(response => {
+            setFoods(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        .then( () => {
+            setWaitingApiResponse(false);
+        }
+        );
+    }
 
     function getDrinkList(){
 
@@ -106,38 +99,39 @@ const Estoque: React.FC = () => {
                 setDrinks(response.data);
             })
             .catch(error => {
-                console.log(error)
-            })
+                console.log(error);
+            });
     }
 
     function toggle(drinkId: string | number){
+
         if(isActive){
-            setActive(false)
-            document.getElementById('drink-' + drinkId)?.classList.add('hidden') ;
+            setActive(false);
+            document.getElementById('drink-' + drinkId)?.classList.add('hidden');
         }else if(!isActive){
-            setActive(true)
-            document.getElementById('drink-' + drinkId)?.classList.remove('hidden') ;
+            setActive(true);
+            document.getElementById('drink-' + drinkId)?.classList.remove('hidden');
         }
         
     }
 
     function removeDrink(drink:any){
-        console.log(drink)
+
+        console.log(drink);
+
         api.delete("/drinks/", drink)
         .then(response => {
-            console.log(response)
+            console.log(response);
           })
           .catch(error => { 
-            console.log(error)
-          })
+            console.log(error);
+          });
     }
 
 
     return(
         <>
             <HeaderComponent user={"manager"} page="stock" />
-
-            
 
             <div className="stock-container">
                 <div className="menu-options">
@@ -147,71 +141,70 @@ const Estoque: React.FC = () => {
                 <h1>Item</h1>
                 <hr />
                     
-                    {waitingApiResponse && <div className="loaderWrapper"><Loader/></div>}
+                {waitingApiResponse && <div className="loaderWrapper"><Loader/></div>}
                     
                 <div id="drinkList" className="hidden">
-                {drinks.map((drink) => {
-                    return <>
-                        <div className="item">
-                            <h2>{drink.productName}</h2>
-                            <FontAwesomeIcon icon={faEye} className="viewButton" onClick={()=> toggle(drink.id)}/>
-                        </div>
-                        <hr className="line" />
-                        <div id={`drink-${drink.id}`} className="modal hidden" >
-                            <div className="modalBackground" onClick={()=> toggle(drink.id)}></div>  
-                            <div className="detalhesContainer">
-                                <div className="detalhesHeader">
-                                    <div>
-                                        <h3>Detalhes do item</h3>
-                                        <h4>{drink.productName}</h4>   
+                    {drinks.map((drink) => {
+                        return <>
+                            <div className="item">
+                                <h2>{drink.productName}</h2>
+                                <FontAwesomeIcon icon={faEye} className="viewButton" onClick={()=> toggle(drink.id)}/>
+                            </div>
+                            <hr className="line" />
+
+                            <div id={`drink-${drink.id}`} className="modal hidden" >
+                                <div className="modalBackground" onClick={()=> toggle(drink.id)}></div>  
+                                <div className="detalhesContainer">
+                                    <div className="detalhesHeader">
+                                        <div>
+                                            <h3>Detalhes do item</h3>
+                                            <h4>{drink.productName}</h4>   
+                                        </div>
+                                        <button className="closeModal" onClick={()=> toggle(drink.id)}>
+                                            <FontAwesomeIcon icon={faX} className="closeButton"/>
+                                        </button> 
                                     </div>
-                                    <button className="closeModal" onClick={()=> toggle(drink.id)}>
-                                        <FontAwesomeIcon icon={faX} className="closeButton"/>
-                                    </button> 
-                                </div>
-                                <div className="atrContainer">
-                                    <h5>Alcoólico</h5>
-                                    {drink.alcoholic ?
-                                        <select disabled className="atr">
-                                            <option value="1">Sim</option>
-                                            <option value="2">Não</option>
-                                        </select> :
-                                        <select disabled className="atr">
-                                            <option value="2">Não</option>
-                                            <option value="1">Sim</option>
-                                        </select> 
-                                    }
-                                    <h5>Preço *</h5>
-                                    <input type="number" min="1" step="any" placeholder={`R$ ${drink.price.toFixed(2)}`} className="atr"/>
-                                    <h5>Observação *</h5>
-                                    <input className="atr" type="text" placeholder={`${drink.description}`} />
-                                    <h5>Quantidade em Estoque *</h5>
-                                    <input type="number" className="atr" placeholder={`${drink.stockAmmount}`}/>
-                                    
-                                    
-                                    
-                                   
-                                </div>
-                                <div className="buttonContainer">
-                                    <button onClick={()=>removeDrink(drink)}>Excluir item</button>
-                                    <button>Atualizar item</button>
+
+                                    <div className="atrContainer">
+                                        <h5>Alcoólico</h5>
+                                        {drink.alcoholic ?
+                                            <select disabled className="atr">
+                                                <option value="1">Sim</option>
+                                                <option value="2">Não</option>
+                                            </select> :
+                                            <select disabled className="atr">
+                                                <option value="1">Sim</option>
+                                                <option selected="selected" value="2">Não</option>
+                                            </select> 
+                                        }
+                                        <h5>Preço *</h5>
+                                        <input type="number" min="1" step="any" placeholder={`R$ ${drink.price.toFixed(2)}`} className="atr"/>
+                                        <h5>Observação *</h5>
+                                        <input className="atr" type="text" placeholder={`${drink.description}`} />
+                                        <h5>Quantidade em Estoque *</h5>
+                                        <input type="number" className="atr" placeholder={`${drink.stockAmmount}`}/>  
+                                    </div>
+
+                                    <div className="buttonContainer">
+                                        <button onClick={()=>removeDrink(drink)}>Excluir item</button>
+                                        <button>Atualizar item</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </>
-                  })}
-                  </div>
+                        </>
+                    })}
+                </div>
 
                 <div id="foodList" className="hidden">
-                {foods.map((food) => {
-                    return <>
-                        <div className="item">
-                            <h2>{food.productName}</h2>
-                            <FontAwesomeIcon icon={faEye} className="viewButton"/>
-                        </div>
-                        <hr />
-                    </>
-                  })}
+                    {foods.map((food) => {
+                        return <>
+                            <div className="item">
+                                <h2>{food.productName}</h2>
+                                <FontAwesomeIcon icon={faEye} className="viewButton"/>
+                            </div>
+                            <hr />
+                        </>
+                    })}
                 </div>
             </div>
         </>
