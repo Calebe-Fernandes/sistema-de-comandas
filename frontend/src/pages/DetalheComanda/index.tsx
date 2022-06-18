8a21d3ee90c27a1a2ecfeaae40492295cfe7d132
Luanda Vital <luavital0@gmail.com>
2022-06-18 11:48:56 -0300

    [style/refactor] Nova comanda, menu produtos e detalhes da comanda Estilização global: - inserção da estilização do scrollbar, modal, hidden, thead fixo e posicionamento relative - alteração do posicionamento e largura do btn Nova comanda: - formatação dos arquivos - exclusão de estilos presentes no arquivo de estilização global Menu produtos: - formatação do arquivo tsx - ajuste da tabela com scrollview e thead fixo - renomeação do arquivo de estilo Command header: - alteração da margem Add button: - alteração para posição fixa Detalhes da comanda: - formatação dos arquivos - ajuste da tabela com scrollview e thead fixo - ajuste do footer da página para garçom e caixa

--- frontend/src/pages/DetalheComanda/index.tsx
+++ frontend/src/pages/DetalheComanda/index.tsx
@@ -1,30 +1,24 @@
 import React, { useEffect, useState } from "react";
-import {useParams} from "react-router"
+import { useParams } from "react-router"
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 
 import { AddButtonComponent, CommandHeaderComponent, Loader } from "../../components";
 import { api } from "../../services/api";
-
 import "./styles.scss";
 
-
-const DetalheComanda:React.FC = () => {
+const DetalheComanda: React.FC = () => {
   const navigate = useNavigate();
   const params = useParams();
 
-
-
   var requestCommand;
-  var [command,setCommand] = useState<any>();
-  var [drinks,setDrinks] = useState<any[]>([]);
-  var [foods,setFoods] = useState<any[]>([]);
+  var [command, setCommand] = useState<any>();
+  var [drinks, setDrinks] = useState<any[]>([]);
+  var [foods, setFoods] = useState<any[]>([]);
   var [checkedDrinks, setCheckedDrinks] = useState<string[]>([]);
   var [checkedFood, setCheckedFood] = useState<string[]>([]);
 
-
-
-  var user : string;
+  var user: string;
   if (window.location.href.indexOf("garcom") > -1) {
     user = "waiter";
   } else if (window.location.href.indexOf("caixa") > -1) {
@@ -34,140 +28,140 @@ const DetalheComanda:React.FC = () => {
   }
 
   useEffect(() => {
-      getOrderItens();
-    },[]);
+    getOrderItens();
+  }, []);
 
   const getOrderItens = () => {
-    api.get(`/order/${params.id}`) 
-        .then(response => {
-          requestCommand = response.data;
-          setCommand(requestCommand);
+    api.get(`/order/${params.id}`)
+      .then(response => {
+        requestCommand = response.data;
+        setCommand(requestCommand);
 
-          requestCommand.drinkWithdrawalList.forEach((drink:any) => {
-            setDrinks(arr => {return [...arr,drink]});
-          });
+        requestCommand.drinkWithdrawalList.forEach((drink: any) => {
+          setDrinks(arr => { return [...arr, drink] });
+        });
 
-          requestCommand.foodWithdrawalList.forEach((food:any) => {
-            setFoods(arr => {return [...arr,food]});
-          });
+        requestCommand.foodWithdrawalList.forEach((food: any) => {
+          setFoods(arr => { return [...arr, food] });
+        });
 
-        })
-        .catch(error => {console.log(error)})
+      })
+      .catch(error => { console.log(error) })
   }
 
-  const navigateToProductMenu = () =>{
+  const navigateToProductMenu = () => {
     navigate(`/garcom/comandas/detalhes/menu/${params.id}`);
   };
 
-  const closeCommand = ()=>{
-    api.put(`/order/close/${params.id}`).then( response => {
-            toast.success('Comanda fechada com sucesso!', {
-              position: "top-center",
-              autoClose: 3000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              });
-              navigate("/caixa/comandas");
-          }
-        ).catch(error => {
-          console.error();
-            toast.error('Um erro inesperado ocorreu', {
-              position: "top-center",
-              autoClose: 3000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              });
-          }
-        );
+  const closeCommand = () => {
+    api.put(`/order/close/${params.id}`).then(response => {
+      toast.success('Comanda fechada com sucesso!', {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      navigate("/caixa/comandas");
+    }
+    ).catch(error => {
+      console.error();
+      toast.error('Um erro inesperado ocorreu', {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    );
   }
 
-  const handleCheckDrinks = (e:any) =>{
-    var input =  document.getElementById(`quantity-for-${e.target.value}`)
+  const handleCheckDrinks = (e: any) => {
+    var input = document.getElementById(`quantity-for-${e.target.value}`)
     var updatedList = [...checkedDrinks]
-    if(e.target.checked){
+    if (e.target.checked) {
       updatedList = [...checkedDrinks, e.target.value];
-        input != null && input.removeAttribute('disabled') 
-    }else{
-    updatedList.splice(checkedDrinks.indexOf(e.target.value), 1);
+      input != null && input.removeAttribute('disabled')
+    } else {
+      updatedList.splice(checkedDrinks.indexOf(e.target.value), 1);
       input != null && input.setAttribute("disabled", "disabled");
       (input as HTMLInputElement).value = '';
     }
     setCheckedDrinks(updatedList);
   }
 
-  function handleCheckFood(e:any){
-    var input =  document.getElementById(`quantity-for-${e.target.value}`)
+  function handleCheckFood(e: any) {
+    var input = document.getElementById(`quantity-for-${e.target.value}`)
     var updatedList = [...checkedFood]
-    if(e.target.checked){
+    if (e.target.checked) {
       updatedList = [...checkedFood, e.target.value];
-      input != null && input.removeAttribute('disabled') 
-    }else{
+      input != null && input.removeAttribute('disabled')
+    } else {
       updatedList.splice(checkedFood.indexOf(e.target.value), 1);
       input != null && input.setAttribute("disabled", "disabled");
       (input as HTMLInputElement).value = '';
     }
     setCheckedFood(updatedList);
   }
-  
+
   const removeItemsFromOrder = () => {
-    if(checkedDrinks.length > 0 || checkedFood.length > 0){
+    if (checkedDrinks.length > 0 || checkedFood.length > 0) {
       checkedDrinks.forEach(drinkID => {
         api.put(`/order/${params.id}/drink-return/${drinkID}`)
-            .then( response => {
-              toast.success('Item removido com sucesso', {
-                position: "top-center",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-              });
-            })
-            .catch( error => {
-              console.log(error);
-              toast.error('Um erro inesperado ocorreu', {
-                position: "top-center",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-              });
+          .then(response => {
+            toast.success('Item removido com sucesso', {
+              position: "top-center",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
             });
+          })
+          .catch(error => {
+            console.log(error);
+            toast.error('Um erro inesperado ocorreu', {
+              position: "top-center",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            });
+          });
       });
       checkedFood.forEach(foodID => {
         api.put(`/order/${params.id}/food-return/${foodID}`)
-            .then( response => {
-              toast.success('Item removido com sucesso', {
-                position: "top-center",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-              });
-            })
-            .catch( error => {
-              console.log(error);
-              toast.error('Um erro inesperado ocorreu', {
-                position: "top-center",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-              });
+          .then(response => {
+            toast.success('Item removido com sucesso', {
+              position: "top-center",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
             });
+          })
+          .catch(error => {
+            console.log(error);
+            toast.error('Um erro inesperado ocorreu', {
+              position: "top-center",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            });
+          });
       });
 
       //Component is not updating, palliative solution to force re-render of the iterms list
@@ -175,7 +169,7 @@ const DetalheComanda:React.FC = () => {
         window.location.reload();
       }, 2500);
 
-    }else{
+    } else {
       toast.warning('Selecione ao menos um item para excluir', {
         position: "top-center",
         autoClose: 2000,
@@ -187,67 +181,81 @@ const DetalheComanda:React.FC = () => {
       });
     }
   }
-   
-    return (
-      <>
-        { command !== undefined && 
-          <div className="command-details-page">
-            <CommandHeaderComponent title={`Mesa  ${command.table}`} />
-            <div className="command-details-container">
-              <div className="delete-item-option-container">
-                <p onClick={removeItemsFromOrder}>Excluir Item</p>
-              </div>
 
-              <table>
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>Item</th>
-                    <th>Quant.</th>
-                  </tr>
-                </thead>
-                <tbody>
-                { drinks.map(drink => {
+  return (
+    <>
+      {command !== undefined &&
+        <div className="command-details-page">
+          <CommandHeaderComponent title={`Mesa  ${command.table}`} />
+          <div className="command-details-container">
+            <p className="delete-item" onClick={removeItemsFromOrder}>Excluir Item</p>
+
+            <div id="drinkList" className="relative-position">
+              <div className="table-container">
+                <table>
+                  <thead className="fixed-thead">
+                    <tr>
+                      <th></th>
+                      <th>Item</th>
+                      <th>Quant.</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {drinks.map(drink => {
                       return (
-                        <>            
+                        <>
                           <tr key={drink.id}>
-                            <td><input type="checkbox" className="selectItemCheckbox" value={drink.id} onChange={handleCheckDrinks}/></td>
+                            <td><input type="checkbox" className="selectItemCheckbox" value={drink.id} onChange={handleCheckDrinks} /></td>
                             <td>{drink.drink.productName}</td>
-                            <td><input disabled type="text" id={`quantity-for-${drink.id}`} className="quantityInput" value={drink.quantity}/></td>
+                            <td>{drink.quantity}</td>
                           </tr>
                         </>
                       )
-                    }
-                  ) 
-                }
-                {
-                  foods.map(food => {
-                    return (
-                      <>            
-                        <tr key={food.id}>
-                          <td><input type="checkbox" className="selectItemCheckbox" value={food.id} onChange={handleCheckFood}/></td>
-                          <td>{food.food.productName}</td>
-                          <td><input disabled type="text" id={`quantity-for-${food.id}`} className="quantityInput" value={food.quantity}/></td>
-                        </tr>
-                      </>
-                    )
-                  }
-                  ) 
-                }
-
-                </tbody>
-              </table>
-              
-              <div className="order-total">
-                <p>R${command.orderTotal}</p>
+                    })}
+                    {foods.map(food => {
+                        return (
+                          <>
+                            <tr key={food.id}>
+                              <td><input type="checkbox" className="selectItemCheckbox" value={food.id} onChange={handleCheckFood} /></td>
+                              <td>{food.food.productName}</td>
+                              <td><input disabled type="text" id={`quantity-for-${food.id}`} className="quantityInput" value={food.quantity} /></td>
+                            </tr>
+                          </>
+                        )
+                    })}
+                  </tbody>
+                </table>
               </div>
-              <AddButtonComponent navigate={navigateToProductMenu}/>
             </div>
-            { user === 'cashier'  &&  <button className="btn closeCommandButton" onClick={closeCommand}>Fechar comanda</button>}           
-          </div>  
-        }   
-      </>
-    )
+
+            {user === 'waiter' &&
+              <div className="footer waiter-footer">
+                <div className="order-total">
+                  <p className="total-text">Total</p>
+                  <p className="value">R$ {command.orderTotal}</p>
+                </div>
+
+                <AddButtonComponent navigate={navigateToProductMenu} />
+              </div>
+            }
+          </div>
+
+          {user === 'cashier' &&
+            <div className="footer cashier-footer">
+              <div className="order-total">
+                <p className="total-text">Total</p>
+                <p className="value">R$ {command.orderTotal}</p>
+              </div>
+
+              <AddButtonComponent navigate={navigateToProductMenu} />
+
+              <button className="btn closeCommandButton" onClick={closeCommand}>Fechar comanda</button>
+            </div>
+          }
+        </div>
+      }
+    </>
+  )
 }
 
 export default DetalheComanda
