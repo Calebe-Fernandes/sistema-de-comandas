523ca4391fe89f227c91f97f983a2c5bc08762d7
brenodupin <breno.dupin@gmail.com>
2023-06-03 19:47:06 -0300

    add endpoint login + token creation

--- backend/src/main/java/com/produtos/apirest/controllers/UserController.java
+++ backend/src/main/java/com/produtos/apirest/controllers/UserController.java
@@ -1,8 +1,10 @@
 package com.produtos.apirest.controllers;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.transaction.Transactional;
 
 import io.jsonwebtoken.Claims;
@@ -35,6 +37,47 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    private static String generateToken(User user) {
+        // Set the expiration time for the token (e.g., 1 day)
+        Date expiration = new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000);
+
+        // Build the token with the user's ID as the subject
+        Key key = new SecretKeySpec("tokenKEY123".getBytes(), SignatureAlgorithm.HS256.getJcaName());
+        String token = Jwts.builder()
+                .setSubject(String.valueOf(user.getId()))
+                .setExpiration(expiration)
+                .signWith(key)
+                .compact();
+
+        return token;
+    }
+
+    private static boolean validatePassword(String pass1, String pass2) {
+        return pass1.equals(pass2);
+    }
+
+    @PostMapping("/login")
+    @ApiOperation(value = "Autentica Usuário e retorna seu token")
+    public ResponseEntity<String> login(@RequestBody User loginUser) {
+        // Retrieve user from the database based on the provided username or email
+        User user = userRepository.findById(loginUser.getId());
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+
+        // Verify the provided password against the stored password hash
+        boolean isPasswordValid = validatePassword(loginUser.getPassword(), user.getPassword());
+
+        if (!isPasswordValid) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+
+        // Generate and return a token (e.g., JWT)
+        String token = generateToken(user);
+        return ResponseEntity.status(HttpStatus.OK).body(token);
+    }
+
     // User operations
     @GetMapping("/users")
     @ApiOperation(value = "Retorna uma lista com todos os usuários cadastrados")
@@ -45,7 +88,7 @@ public class UserController {
     @PostMapping("/user")
     @Transactional
     @ApiOperation(value = "Cria um novo usuário")
-    public ResponseEntity<User> createUser(@RequestBody @Validated User newUser){
+    public ResponseEntity<User> createUser(@RequestBody @Validated User newUser) {
         Date dateNow = new Date();
         newUser.setCreatedAt(dateNow);
         newUser.setUpdatedAt(dateNow);
@@ -56,7 +99,7 @@ public class UserController {
     @PutMapping("/user")
     @Transactional
     @ApiOperation(value = "Atualiza as informações de um usuário.")
-    public ResponseEntity<User> updateUser(@RequestBody @Validated User updatedUser){
+    public ResponseEntity<User> updateUser(@RequestBody @Validated User updatedUser) {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(userRepository.save(updatedUser));
     }
 }
