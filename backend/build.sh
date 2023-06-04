7c490af43997a734cd853ff393c2b27a7d85ca53
brenodupin <breno.dupin@gmail.com>
2023-06-03 22:44:06 -0300

    exclui img antiga depois que faz a nova

--- backend/build.sh
+++ backend/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CONTAINER_APP="backend-app-1"
+CONTAINER_APP="backend-app"
 
 # Check if the container is running
-if docker ps --filter "name=${CONTAINER_APP}" --format "{{.Names}}" | grep -q "${CONTAINER_APP}"; then
+if docker ps --filter "name=${CONTAINER_APP}-1" --format "{{.Names}}" | grep -q "${CONTAINER_APP}-1"; then
     echo "Stopping and removing container: ${CONTAINER_APP}"
-    sudo docker stop ${CONTAINER_APP}
-    sudo docker rm ${CONTAINER_APP}
+    sudo docker stop ${CONTAINER_APP}-1
+    sudo docker rmi ${CONTAINER_APP} --force
 else
     echo "Container ${CONTAINER_NAME} is not running."
 fi
