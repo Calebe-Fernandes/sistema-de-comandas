a00d65243980c6f92a4adf968b144c8c67777f9a
brenodupin <breno.dupin@gmail.com>
2023-06-03 22:19:46 -0300

    login basico funcionando

--- backend/src/main/java/com/produtos/apirest/controllers/UserController.java
+++ backend/src/main/java/com/produtos/apirest/controllers/UserController.java
@@ -8,7 +8,9 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.transaction.Transactional;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +22,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.produtos.apirest.exceptions.ApiRequestException;
 import com.produtos.apirest.models.User;
 import com.produtos.apirest.repository.UserRepository;
 
@@ -37,19 +41,46 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    private static String generateToken(User user) {
-        // Set the expiration time for the token (e.g., 1 day)
-        Date expiration = new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000);
+    static Key UserControllerKey = new SecretKeySpec("tokenKEY12345678910tokenKEY12345678910".getBytes(),
+            SignatureAlgorithm.HS256.getJcaName());
 
-        // Build the token with the user's ID as the subject
-        Key key = new SecretKeySpec("tokenKEY123".getBytes(), SignatureAlgorithm.HS256.getJcaName());
+    private static String generateToken(User user) {
+        // expiration time for the token (1 day = 1000(ms) * 60(s) * 60(min) * 24(h))
+        Date expiration = new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24);
+
         String token = Jwts.builder()
                 .setSubject(String.valueOf(user.getId()))
                 .setExpiration(expiration)
-                .signWith(key)
+                .signWith(UserControllerKey)
                 .compact();
 
         return token;
+    }
+
+    private static long BearerTokenGetUserId(String bearerToken) {
+        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
+            throw new ApiRequestException("Não foi enviado Bearer");
+        }
+        System.out.println("token: " + bearerToken);
+
+        try {
+            String token = bearerToken.substring(7); // Extract the token from the Authorization header
+
+            // Validate and parse the token
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(UserControllerKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return Integer.parseInt(claims.getSubject());
+        } catch (ExpiredJwtException exp) {
+            throw new ApiRequestException("Token expired: " + exp.getMessage());
+        } catch (MalformedJwtException inv) {
+            throw new ApiRequestException("Invalid token: " + inv.getMessage());
+        } catch (Exception e) {
+            throw new ApiRequestException("Error while processing token: " + e.getMessage());
+        }
     }
 
     private static boolean validatePassword(String pass1, String pass2) {
@@ -59,30 +90,28 @@ public class UserController {
     @PostMapping("/login")
     @ApiOperation(value = "Autentica Usuário e retorna seu token")
     public ResponseEntity<String> login(@RequestBody User loginUser) {
-        // Retrieve user from the database based on the provided username or email
-        User user = userRepository.findById(loginUser.getId());
+        // Retrieve user from the database based on the provided username
+        User user = userRepository.findByUsername(loginUser.getUsername());
 
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-        }
+        if (user == null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials (user)");
 
-        // Verify the provided password against the stored password hash
-        boolean isPasswordValid = validatePassword(loginUser.getPassword(), user.getPassword());
+        if (!validatePassword(loginUser.getPassword(), user.getPassword()))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials (password)");
 
-        if (!isPasswordValid) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-        }
-
-        // Generate and return a token (e.g., JWT)
+        // Generate and return a token
         String token = generateToken(user);
+        System.out.println("token out? " + token);
         return ResponseEntity.status(HttpStatus.OK).body(token);
     }
 
     // User operations
     @GetMapping("/users")
     @ApiOperation(value = "Retorna uma lista com todos os usuários cadastrados")
-    public List<User> listUsers() {
-        return userRepository.findAll();
+    public ResponseEntity<List<User>> listUsers(@RequestHeader("Authorization") String bearerToken) {
+        // gambiara, só pra ver ser o login teve sucesso
+        long a = BearerTokenGetUserId(bearerToken);
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
     }
 
     @PostMapping("/user")
